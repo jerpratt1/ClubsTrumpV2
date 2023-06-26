865be9a8007ef6248f4f3bbc752db2726f93d664
Jeremy Pratt <jerpratt1@gmail.com>
2023-06-26 17:18:20 -0400

    Create ScoreCard.vue

---
+++ src/components/ScoreCard.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1 v-for="player in roster" v-bind:key = player.id></h1>
+  </div>
+</template>
+
+<script>
+export default {
+data() {
+    return {
+      scoreCard: [],
+      roster: []
+      ,
+    };
+  },
+}
+</script>
+
+<style>
+
+</style>

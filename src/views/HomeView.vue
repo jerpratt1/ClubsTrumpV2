8ad0264ccfe5f514df5b0217dd18598502182136
Jeremy Pratt <jerpratt1@gmail.com>
2023-07-06 17:46:11 -0400

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,30 @@
 <template>
   <div class="home">
-    This is the landing page
+    <h2>A Game of Skill & Chance</h2>
+    <h1>Clubs Trump</h1>
+    <img src="https://images.pexels.com/photos/279009/pexels-photo-279009.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="cards">
   </div>
 </template>
 
 <script>
-
-
 export default {
 }
 </script>
+
+<style scoped>
+
+.home{
+background-color: #2B7A78;
+background-color: rgba(0, 0, 0, .25);
+color: #FEFFFF;
+max-width: 70%;
+margin: auto;
+padding-bottom: 5%;
+padding-top: 1%;
+}
+
+img{
+  border-radius: 5px;
+  max-width: 30%;
+}
+</style>
